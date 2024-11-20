e052682cce8b2badcb549ea91dd818c13217d139
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2024-11-19 19:21:50 -0700

    header title correspond to routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -11,6 +12,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const segments = useSegments();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -26,12 +28,20 @@ export default function RootLayout() {
     return null;
   }
 
+  const screenTitles: Record<string, string> = {
+    car: "Car Details",
+    maintenance: "Maintenance",
+    gaslog: "Gas Log"
+  }
+
+  const activeScreen = segments[segments.length - 1];
+  const headerTitle = screenTitles[activeScreen] || "Default Screen";
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ title: 'All Cars', headerShown: false }} />
         <Stack.Screen name="addcar" options={{ title: 'Add New Car', headerShown: true }} />
-        <Stack.Screen name="screens" options={{ title: '', headerShown: true }} />
+        <Stack.Screen name="screens" options={{ title: headerTitle, headerShown: true }} />
         <Stack.Screen name="+not-found"  />
       </Stack>
     </ThemeProvider>
